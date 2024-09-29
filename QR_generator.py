d0cbdf9edbbfb4f499b5b48edabc1fdca7d303a5
Art_Phy <155635151+Art-Phy@users.noreply.github.com>
2024-09-29 18:47:35 +0200

    QR_generator.py

--- QR_generator.py
+++ QR_generator.py
@@ -8,7 +8,7 @@ import qrcode
 from PIL import Image, ImageDraw, ImageFont
 
 # introducimos lo que queremos que se encuentre en el QR
-data = "https://discord.com/channels/1288084026883706912/1288084028611629148"
+data = "pon aquí tu ruta"
 
 # definimos las características del QR
 qr = qrcode.QRCode(version=8, box_size=7, border=3)
@@ -19,7 +19,7 @@ qr.make(fit=True)
 img = qr.make_image(fill_color = 'black', back_color = 'white').convert('RGB')
 
 # le indicamos nuestro logo y sus dimensiones
-logo = Image.open("/Users/arturosanma/Desktop/Informática/Logo.png")
+logo = Image.open("pon aquí la ruta donde tienes guardado tu logo")
 
 logo_size = 120
 logo = logo.resize((logo_size, logo_size))
@@ -37,4 +37,4 @@ logo_position = ((qr_width - logo_size) // 2, (qr_height - logo_size) // 2)
 img.paste(logo, logo_position, mask)
 
 # guardamos nuestro QR en la ubicación que queramos
-img.save("/Users/arturosanma/Desktop/qr_image.png")
+img.save("/?/?/?/qr_image.png")
